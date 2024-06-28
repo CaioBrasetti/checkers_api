55c9663b6957c96e04fef16bc954a3615c6df7d4
Caio Brasetti <caiobrasetti@hotmail.com>
2024-06-28 12:11:52 -0300

    correcting the creation of tokens

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,12 @@
 class Game < ApplicationRecord
-  before_create :generate_token
+  before_create :generate_tokens_to_players
+
+  def generate_tokens_to_players
+    self.player1_token = generate_token
+    self.player2_token = generate_token
+  end
 
   def generate_token
-    self.token = TokenService.generate_token
+    TokenService.generate_token
   end
 end
