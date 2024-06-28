06dd5dba6ee61dcbee9eba23638e1ba976b7d961
Caio Brasetti <caiobrasetti@hotmail.com>
2024-06-28 16:54:20 -0300

    Correcting the token service

--- app/services/token_service.rb
+++ app/services/token_service.rb
@@ -1,15 +1,9 @@
 class TokenService
-  SECRET_KEY = Rails.application.credentials.secret_key_base
-
   def self.generate_token
-    payload = SecureRandom.hex(10)
-    JWT.encode(payload, SECRET_KEY, 'HS256')
+    SecureRandom.hex(32)
   end
 
-  def self.decode_token(token)
-    decoded = JWT.decode(token, SECRET_KEY)[0]
-    HashWithIndifferentAccess.new(decoded)
-  rescue JWT::DecodeError
-    nil
+  def self.validade_token(token_player, token_game)
+    token_player == token_game
   end
 end
