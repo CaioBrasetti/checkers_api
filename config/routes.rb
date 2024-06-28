bc211e71a974492ea005dac77e65fb849975df11
Caio Brasetti <caiobrasetti@hotmail.com>
2024-06-28 16:55:11 -0300

    Adding join_game route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'games/test_token', to: 'games#test_token'
-  resources :games, only: [:create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
   # Defines the root path route ("/")
   # root "posts#index"
+
+  get "up" => "rails/health#show", as: :rails_health_check
+  resources :games, only: [:create]
+
+  post '/join_game', to: 'games#join_game'
 end
