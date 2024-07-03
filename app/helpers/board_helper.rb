d9077feec772cd27595ddf87c7c75decff44f321
Caio Brasetti <caiobrasetti@hotmail.com>
2024-07-03 12:14:29 -0300

    Updating board and creating new rules

--- app/helpers/board_helper.rb
+++ app/helpers/board_helper.rb
@@ -15,26 +15,42 @@ module BoardHelper
 
     def move(game, old_position, new_position, player_color)
       @board = JSON.parse(game.board)
+      allowed_position = RulesHelper.captured_piece(@board, player_color)
+
+      unless allowed_position.nil?
+        return invalid_movement unless allowed_position[:new_position] == new_position
+      end
+
       old_row, old_col = old_position.split(',').map(&:to_i) # ["2", "3"] { |s| s.to_i }
       new_row, new_col = new_position.split(',').map(&:to_i)
-
       piece = @board[old_row][old_col]
 
-      return invalid_movement unless piece.slice(0) == player_color
+      return invalid_movement unless piece == player_color
 
       if piece.include?('K')
-        return invalid_movement unless RulesHelper.valid_king_move?(@board, old_row, old_col, new_row, new_col)
+        # return invalid_movement unless RulesHelper.valid_king_move?(@board, old_row, old_col, new_row, new_col)
 
-        update_board(old_row, old_col, new_row, new_col, piece)
-        valid_movement("Movimento realizado de #{old_position} para #{new_position}")
+        # update_board(old_row, old_col, new_row, new_col, piece)
+        # valid_movement("Movimento realizado de #{old_position} para #{new_position}")
       else
         up_down = player_color == 'W' ? -1 : 1
-        response = RulesHelper.valid_move?(@board, up_down, new_row, new_col, old_row, old_col)
+        row = nil
+        col = nil
+
+        if allowed_position
+          piece_position = allowed_position[:piece_position].split(',').map(&:to_i)
+          new_position = allowed_position[:new_position].split(',').map(&:to_i)
+
+          row = (piece_position[0] - new_position[0]).abs
+          col = (piece_position[1] - new_position[1]).abs
+        end
+
+        response = RulesHelper.valid_move?(@board, up_down, new_row, new_col, old_row, old_col, row, col)
 
         return invalid_movement unless response
 
         promote_to_king(new_row, new_col, player_color)
-        update_board(old_row, old_col, new_row, new_col, piece)
+        update_board(old_row, old_col, new_row, new_col, piece, allowed_position)
         valid_movement("Movimento realizado de #{old_position} para #{new_position}")
       end
     end
@@ -49,9 +65,13 @@ module BoardHelper
       { error: 'Movimento inválido: movimento não permitido pelas regras', status: :unprocessable_entity }
     end
 
-    def update_board(old_row, old_col, new_row, new_col, piece)
+    def update_board(old_row, old_col, new_row, new_col, piece, allowed_position)
       @board[old_row][old_col] = 1
       @board[new_row][new_col] = piece
+
+      captured_row, captured_col = allowed_position[:captured_position].split(',').map(&:to_i) if allowed_position.present?
+      @board[captured_row][captured_col] = 1 if allowed_position.present?
+
       @board
     end
 
--- app/helpers/rules_helper.rb
+++ app/helpers/rules_helper.rb
@@ -1,21 +1,47 @@
 module RulesHelper
   class << self
-    def valid_move?(board, up_down, new_row, new_col, old_row, old_col)
-      return unless new_col >= 0 && new_row >= 0
+    def captured_piece(board, color)
+      captured_positions = []
 
-      row_diff = new_row - old_row
-      col_diff = (new_col - old_col).abs
+      directions = [
+        { row_offset: 1, col_offset: 1, capture_row_offset: 2, capture_col_offset: 2 },
+        { row_offset: 1, col_offset: -1, capture_row_offset: 2, capture_col_offset: -2 },
+        { row_offset: -1, col_offset: 1, capture_row_offset: -2, capture_col_offset: 2 },
+        { row_offset: -1, col_offset: -1, capture_row_offset: -2, capture_col_offset: -2 }
+      ]
 
-      up_or_down?(row_diff, up_down) &&
-        diagonal?(col_diff) &&
-        empty_position?(board, new_row, new_col)
+      board_size = board.size
+
+      board.each_with_index do |row, old_row|
+        row.each_with_index do |cell, old_col|
+          next unless cell == color
+
+          opponent_color = color == 'B' ? 'W' : 'B'
+
+          directions.each do |dir|
+            next unless valid_position?(old_row + dir[:row_offset], old_col + dir[:col_offset], board_size) &&
+                        valid_position?(old_row + dir[:capture_row_offset], old_col + dir[:capture_col_offset], board_size)
+
+            if board[old_row + dir[:row_offset]][old_col + dir[:col_offset]] == opponent_color &&
+               board[old_row + dir[:capture_row_offset]][old_col + dir[:capture_col_offset]] == 1
+
+              captured_row = (old_row + (old_row + dir[:capture_row_offset])) / 2
+              captured_col = (old_col + (old_col + dir[:capture_col_offset])) / 2
+
+              captured_positions << {
+                piece_position: "#{old_row},#{old_col}",
+                new_position: "#{old_row + dir[:capture_row_offset]},#{old_col + dir[:capture_col_offset]}",
+                captured_position: "#{captured_row},#{captured_col}"
+              }
+            end
+          end
+        end
+      end
+      captured_positions.empty? ? nil : captured_positions.first
     end
 
-    def valid_king_move?(board, old_row, old_col, new_row, new_col)
-      row_diff = (new_row - old_row).abs
-      col_diff = (new_col - old_col).abs
-
-      row_diff == col_diff && board[new_row][new_col] == 1
+    def valid_position?(row, col, board_size)
+      row.between?(0, board_size - 1) && col.between?(0, board_size - 1)
     end
 
     def allowed_positions(board, old_position, color)
@@ -36,7 +62,7 @@ module RulesHelper
       ]
 
       possible_positions.each do |new_row, new_col|
-        moves << [new_row, new_col] if valid_move?(board, up_down, new_row, new_col, old_row, old_col)
+        moves << [new_row, new_col] if valid_move?(board, up_down, new_row, new_col, old_row, old_col, nil, nil)
       end
 
       moves
@@ -62,14 +88,35 @@ module RulesHelper
       moves
     end
 
-    private
+    def valid_king_move?(board, old_row, old_col, new_row, new_col)
+      row_diff = (new_row - old_row).abs
+      col_diff = (new_col - old_col).abs
 
-    def up_or_down?(row_diff, up_down)
-      row_diff == up_down
+      row_diff == col_diff && board[new_row][new_col] == 1
     end
 
-    def diagonal?(col_diff)
-      col_diff == 1
+    def valid_move?(board, up_down, new_row, new_col, old_row, old_col, row, col)
+      return unless new_col >= 0 && new_row >= 0
+
+      diagonal = col.present? ? col : 1
+      row_or_up_down = row.present? ? row : up_down
+
+      row_diff = (new_row - old_row).abs
+      col_diff = (new_col - old_col).abs
+
+      row_or_up_down?(row_diff, row_or_up_down) &&
+        diagonal?(col_diff, diagonal) &&
+        empty_position?(board, new_row, new_col)
+    end
+
+    private
+
+    def row_or_up_down?(row_diff, row_or_up_down)
+      row_diff == row_or_up_down.abs
+    end
+
+    def diagonal?(col_diff, diagonal)
+      col_diff == diagonal
     end
 
     def empty_position?(board, new_row, new_col)
